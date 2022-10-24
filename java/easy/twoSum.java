0a3c423b9f16ed2a1a348e73649bb3f24b323003
Casimir Peterson <kazaugp02@gmail.com>
2022-10-23 21:21:14 -0500

    initialCommit

---
+++ java/easy/twoSum.java
@@ -0,0 +1,19 @@
+class twoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int indx1 = 0, indx2 = 0;
+        
+        for (int i = 0; i < nums.length; i++)
+        {
+            for (int j = i + 1; j < nums.length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    indx1 = i;
+                    indx2 = j;
+                } 
+            }
+        }
+        int[] ans = {indx1, indx2};
+        return ans;
+    }
+}
