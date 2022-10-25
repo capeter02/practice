353881d1149d99519654e10deb8ad9c0bf27ab8a
Casimir Peterson <kazaugp02@gmail.com>
2022-10-25 18:05:16 -0500

    dailyLeetCode

---
+++ java/easy/palindromeNumber.java
@@ -0,0 +1,17 @@
+class palidromeNumber {
+    public boolean isPalindrome(int x) {
+        int reversed = 0;
+        int temp = x;
+        
+        while (temp != 0)
+        {
+            int last = temp % 10;
+            reversed = reversed * 10 + last;
+            
+            temp /= 10;
+        }
+        
+        if (reversed != x || reversed < 0) return false;
+        else return true;
+    }
+}
